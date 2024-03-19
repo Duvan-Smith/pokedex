01ce6e616a8b8ba5fc179551ea399ca62cb986f5
Duvan Roque <dsmith.mr@gmail.com>
2024-03-18 22:29:09 -0500

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v2');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -16,6 +18,15 @@ async function bootstrap() {
       }
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Pokemon example')
+    .setDescription('The pokemon API description')
+    .setVersion('1.0')
+    .addTag('Pokemon')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
   console.log(`App running on port ${process.env.PORT}`);
